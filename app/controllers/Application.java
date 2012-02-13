4961143bcf8432fd77196e232668b5940f46f8dd
sakashushu <sakashushu@gmail.com>
2012-02-14 01:02:12 +0900

    テーブル「Records」→「Record」に変更。「明細表」の表示等を試行錯誤。

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,6 +3,8 @@ package controllers;
 import play.*;
 import play.mvc.*;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.util.*;
 
 import models.*;
@@ -13,9 +15,35 @@ public class Application extends Controller {
         render();
     }
 
-    
-    public static void detailList() {
-        render();
+    public static void detailList(String yearFr) {
+    	
+//    	Date date = null;
+//		try {
+//			date = DateFormat.getDateInstance().parse("2012/2/13");
+//		} catch (ParseException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//    	Record record = new Record(date, 1, "食費");
+//    	record.save();
+    	
+    	
+    	String sQuely = "";
+    	if(yearFr == null) {
+    		Calendar calendar = Calendar.getInstance();
+    		sQuely = "payment_date >= '" + String.format("%1$tY/%1$tm/%1$td", calendar.getTime()) + "'";
+    	} else {
+    		sQuely = "payment_date >= '" + yearFr + "/01/01'";
+    	}
+    	sQuely += " order by payment_date desc";
+    	
+    	
+    	List<Record> records = Record.find(
+    			sQuely).from(0).fetch(50);
+        render(records, yearFr);
     }
 	
+    public static void dtlSrch(
+    		) {
+    }
 }
--- app/models/Record.java
+++ app/models/Record.java
@@ -7,20 +7,17 @@ import javax.persistence.Entity;
 import play.db.jpa.Model;
 
 @Entity
-public class Records extends Model {
+public class Record extends Model {
 
 	public Date payment_date;
-	public String payment_time;
-	public Integer item_id;
+	public int item_id;
 	public String item_name;
 	
-	public Records(
+	public Record(
 			Date payment_date,
-			String payment_time,
-			Integer item_id,
+			int item_id,
 			String item_name) {
 		this.payment_date = payment_date;
-		this.payment_time = payment_time;
 		this.item_id = item_id;
 		this.item_name = item_name;
 	}
