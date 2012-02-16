7b32f25db6e32a9bf8de9ccf5ac77ff149409466
sakashushu <sakashushu@gmail.com>
2012-02-16 23:29:23 +0900

    jQuery UIのDatepicker を組み込んでみた

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -15,25 +15,24 @@ public class Application extends Controller {
         render();
     }
 
-    public static void detailList(String yearFr, String yearTo) {
+    public static void detailList(String dateFr, String yearTo) {
     	String sQuely = "";
-    	if(yearFr == null) {
+    	if(dateFr == null) {
     		Calendar calendar = Calendar.getInstance();
     		yearTo = String.format("%1$tY", calendar.getTime());
     		calendar.add(Calendar.YEAR, -1);
-//    		yearFr = String.format("%1$tY/%1$tm/%1$td", calendar.getTime());
-    		yearFr = String.format("%1$tY", calendar.getTime());
+    		dateFr = String.format("%1$tY/%1$tm/%1$td", calendar.getTime());
     	}
-   		sQuely = "payment_date between '" + yearFr + "/01/01' and '" + yearTo + "/12/31'";
+   		sQuely = "payment_date between '" + dateFr + "' and '" + yearTo + "/12/31'";
     	sQuely += " order by payment_date desc";
     	
     	List<Record> records = Record.find(
     			sQuely).from(0).fetch(50);
     	
-    	String yFr = yearFr;
+    	String dFr = dateFr;
     	String yTo = yearTo;
     	
-    	render(records, yFr, yTo);
+    	render(records, dFr, yTo);
     }
 	
     public static void dtlSrch(String yearFr) {
