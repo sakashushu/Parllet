f5355b3b0a33a8db4335f41ef5f407d74adb7ebe
sakashushu <sakashushu@gmail.com>
2012-03-16 15:37:35 +0900

    「PlayPJAX」を組み込み

---
+++ app/controllers/Pjax.java
@@ -0,0 +1,39 @@
+package controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import play.mvc.Controller;
+
+public class Pjax extends Controller {
+	public static void index() {
+		String data1 = params.get("data1");
+		
+		
+		if(data1 == null) {
+			render();
+		} else {
+			List<String> results = new ArrayList<String>();
+			for(int i = 0; i < 10; i++) {
+				results.add("data" + i);
+			}
+			
+			if(isPjax()) {
+				render("/tags/result.html", results);
+			} else {
+				results.add("directCalled");
+				render(results);
+			}
+		}
+	}
+	
+	private static boolean isPjax() {
+		if(params._contains("_pjax")) {
+			return true;
+		} else if(request.headers.containsKey("X-PJAX")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
