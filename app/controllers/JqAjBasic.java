414ad4562605ba19ca822ef3100b6f95eda0e622
sakashushu <sakashushu@gmail.com>
2012-03-18 17:03:34 +0900

    明細表に行追加ボタンを実装中

---
+++ app/controllers/JqAjBasic.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class JqAjBasic extends Controller {
+    public static void two() {
+        render();
+    }
+
+    public static void sample2_load() {
+    	render();
+    }
+
+    public static void three() {
+        render();
+    }
+}
--- app/controllers/Pjax.java
+++ app/controllers/Pjax.java
@@ -36,11 +36,4 @@ public class Pjax extends Controller {
 			return false;
 		}
 	}
-	
-	public static void test() {
-		List<String> results = new ArrayList<String>();
-		results.add("test1");
-		results.add("test2");
-		render(results);
-	}
 }
