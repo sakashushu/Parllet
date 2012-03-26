a471d3121ec769276525dfb5c5fb36283a6893b4
sakashushu <sakashushu@gmail.com>
2012-03-26 17:02:35 +0900

    「PlayFramework1.2.4におけるWebSocket」も組込み中

--- app/controllers/Echo.java
+++ app/controllers/Echo.java
@@ -18,11 +18,14 @@ public class Echo extends Controller {
 			while(inbound.isOpen()) {
 				// クライアントから送られるメッセージを、継続を使って非同期で待ちます。
 				Http.WebSocketEvent event = await(inbound.nextEvent());
+				Http.WebSocketFrame frame = (Http.WebSocketFrame)event;
+
 				// メッセージがテキストであればfor内が実行されます。
 				// パターンマッチにfor文を使うのは珍しいですね。
 				for(String message : TextFrame.match(event)) {
 					// クライアントにメッセージを返送します。
-					outbound.send(message);
+					//outbound.send(message);
+					outbound.send("Echo: %s", frame.textData);
 					
 					// 本題のechoサーバとは何の関係もありませんが、
 					// このように、HTTP用のControllerから利用していたModelをWebSocketControllerからも普通に利用することができます。Interoperability!
