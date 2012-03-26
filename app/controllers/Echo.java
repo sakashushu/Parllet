c25c54330cfd4032970f3988083c446bb64f07d1
sakashushu <sakashushu@gmail.com>
2012-03-26 12:29:36 +0900

    WebSocket Demo を組込み

---
+++ app/controllers/Echo.java
@@ -0,0 +1,34 @@
+package controllers;
+
+import models.LoggedMessage;
+import play.mvc.Controller;
+import play.mvc.Http;
+import play.mvc.WebSocketController;
+
+import static play.mvc.Http.WebSocketEvent.TextFrame;
+
+public class Echo extends Controller {
+	public static void demo() {
+		render();
+	}
+	
+	public static class WebSocketEcho extends WebSocketController {
+		public static void listen() {
+			// WebSocketが接続されている間、isbound.isOpen()はtrue
+			while(inbound.isOpen()) {
+				// クライアントから送られるメッセージを、継続を使って非同期で待ちます。
+				Http.WebSocketEvent event = await(inbound.nextEvent());
+				// メッセージがテキストであればfor内が実行されます。
+				// パターンマッチにfor文を使うのは珍しいですね。
+				for(String message : TextFrame.match(event)) {
+					// クライアントにメッセージを返送します。
+					outbound.send(message);
+					
+					// 本題のechoサーバとは何の関係もありませんが、
+					// このように、HTTP用のControllerから利用していたModelをWebSocketControllerからも普通に利用することができます。Interoperability!
+					new LoggedMessage(message);
+				}
+			}
+		}
+	}
+}
