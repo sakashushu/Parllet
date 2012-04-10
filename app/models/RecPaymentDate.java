a02a49c17a5e1fcf7c6c4c6b02ce12709f4ef9fa
sakashushu <sakashushu@gmail.com>
2012-04-10 15:33:33 +0900

    websocket への送信データ微調整。IEで4/5近辺がうまくいかない？

--- app/models/RecPaymentDate.java
+++ app/models/RecPaymentDate.java
@@ -3,12 +3,14 @@ package models;
 import java.util.Date;
 
 public class RecPaymentDate {
+	public String act_type;
 	public Long id;
 	public String payment_date;
 	
 	public RecPaymentDate() {}
 	
-	public RecPaymentDate(Long id, String payment_date) {
+	public RecPaymentDate(String act_type, Long id, String payment_date) {
+		this.act_type = act_type;
 		this.id = id;
 		this.payment_date= payment_date;
 	}
