ebbaf5da7d575cb6102df78695db7b0de4b2734a
sakashushu <sakashushu@gmail.com>
2012-06-22 15:32:38 +0900

    ファイルのエクスポートは、一旦 renderBinary を使う方向で進めている。

--- app/controllers/Config.java
+++ app/controllers/Config.java
@@ -1,6 +1,10 @@
 package controllers;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.UnsupportedEncodingException;
+
+import models.Record;
 
 import play.mvc.Controller;
 import play.mvc.With;
@@ -18,5 +22,13 @@ public class Config extends Controller {
 			csv.renameTo(saveTo);
 		}
 
+	}
+	
+	public static void download() throws UnsupportedEncodingException {
+		//response.contentType = "text/csv";
+//		final Record record = Record.findById(412); 
+		response.setContentTypeIfNotSet("application/binary");
+		java.io.InputStream binaryData = new ByteArrayInputStream("Hello日本語".getBytes("utf-8"));
+		renderBinary(binaryData);	
 	}
 }
