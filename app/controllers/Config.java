846856546896d744f109b10e4dd5d08c375a0ff2
sakashushu <sakashushu@gmail.com>
2012-06-24 16:58:59 +0900

    ファイルエクスポート機能編集中

--- app/controllers/Config.java
+++ app/controllers/Config.java
@@ -1,9 +1,11 @@
 package controllers;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.ByteArrayInputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.List;
 
+import models.HaUser;
 import models.Record;
 
 import play.mvc.Controller;
@@ -24,11 +26,28 @@ public class Config extends Controller {
 
 	}
 	
-	public static void download() throws UnsupportedEncodingException {
-		//response.contentType = "text/csv";
-//		final Record record = Record.findById(412); 
+	public static void download(
+    		String down_date_fr,	/* 絞込日時範囲（開始） */
+    		String down_date_to		/* 絞込日時範囲（終了） */
+			) throws UnsupportedEncodingException {
+		
+		if((down_date_fr==null) || (down_date_fr.equals(""))) {
+			down_date_fr = "1900/01/01";
+		}
+		if((down_date_to==null) || (down_date_to.equals(""))) {
+			down_date_to = "2999/12/31";
+		}
+		
+		String sOutCsv = "";
+		List<Record> records = Record.find(" payment_date between '" + down_date_fr + "' and '" + down_date_to + "'").fetch();
+		for(Record rec : records) {
+			sOutCsv += "\"" + String.format("%1$tY/%1$tm/%1$td", rec.payment_date) + "\"" +
+					System.getProperty("line.separator")	//改行
+					;
+		}
+		
 		response.setContentTypeIfNotSet("application/binary");
-		java.io.InputStream binaryData = new ByteArrayInputStream("Hello日本語".getBytes("utf-8"));
-		renderBinary(binaryData);	
+		java.io.InputStream binaryData = new ByteArrayInputStream(sOutCsv.getBytes("utf-8"));
+		renderBinary(binaryData, "testName.csv");	
 	}
 }
--- public/javascripts/jquery.datepicker.js
+++ public/javascripts/jquery.datepicker.js
@@ -11,3 +11,27 @@ jQuery(function() {
 		dateFormat: 'yy/mm/dd'
 	});
 });
+
+jQuery(function() {
+	var dates = jQuery("#from, #to").datepicker({
+		defaultDate: "+1w",
+		changeMonth: true,
+		numberOfMonths: 1,
+		showOtherMonths: true,
+		selectOtherMonths: true,
+		showAnim: "drop",
+		changeMonth: true,
+		changeYear: true,
+		yearRange: '1900:2999',
+		dateFormat: 'yy/mm/dd',
+		onSelect: function( selectedDate ) {
+			var option = this.id == "from" ? "minDate" : "maxDate",
+				instance = jQuery( this ).data( "datepicker" ),
+				date = jQuery.datepicker.parseDate(
+					instance.settings.dateFormat ||
+					jQuery.datepicker._defaults.dateFormat,
+					selectedDate, instance.settings );
+			dates.not( this ).datepicker( "option", option, date );
+		}
+	});
+});
