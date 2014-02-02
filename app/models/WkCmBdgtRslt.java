b40c99a12507effe34fd56805dbf6e88ceddd40b
sakashushu <sakashushu@gmail.com>
2014-02-02 23:00:47 +0900

    日計表の予算の更新の submit をやめてjquery.post() に（微調整）

---
+++ app/models/WkCmBdgtRslt.java
@@ -0,0 +1,32 @@
+package models;
+
+public class WkCmBdgtRslt {
+	private int intRslt;
+	private String strErr;
+	private Budget bG;
+	private Integer intSum;
+	public int getIntRslt() {
+		return intRslt;
+	}
+	public void setIntRslt(int intRslt) {
+		this.intRslt = intRslt;
+	}
+	public String getStrErr() {
+		return strErr;
+	}
+	public void setStrErr(String strErr) {
+		this.strErr = strErr;
+	}
+	public Budget getbG() {
+		return bG;
+	}
+	public void setbG(Budget bG) {
+		this.bG = bG;
+	}
+	public Integer getIntSum() {
+		return intSum;
+	}
+	public void setIntSum(Integer intSum) {
+		this.intSum = intSum;
+	}
+}
