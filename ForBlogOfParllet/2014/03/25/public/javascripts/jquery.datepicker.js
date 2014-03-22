4ba59dceabb833bc70f82e6c9b4a280117bbd3e2
sakashushu <sakashushu@gmail.com>
2014-03-22 22:58:10 +0900

    ForBlogOfParllet に 2014/03/25 を追加・2014/03/24 の微調整

---
+++ ForBlogOfParllet/2014/03/25/public/javascripts/jquery.datepicker.js
@@ -0,0 +1,64 @@
+jQuery(function() {
+	var dates = jQuery("#from, #to").datepicker({
+		defaultDate: "+1w",
+		changeMonth: true,
+		numberOfMonths: 1,
+		showOtherMonths: true,
+		selectOtherMonths: true,
+		showAnim: "drop",
+		changeMonth: true,
+		changeYear: true,
+		yearRange: '1900:2999',
+		dateFormat: 'yy/mm/dd',
+		onSelect: function( selectedDate ) {
+			var option = this.id == "from" ? "minDate" : "maxDate",
+				instance = jQuery( this ).data( "datepicker" ),
+				date = jQuery.datepicker.parseDate(
+					instance.settings.dateFormat ||
+					jQuery.datepicker._defaults.dateFormat,
+					selectedDate, instance.settings );
+			dates.not( this ).datepicker( "option", option, date );
+		},
+		showButtonPanel: true
+	});
+});
+
+jQuery(function() {
+	var dates = jQuery("#from2, #to2").datepicker({
+		defaultDate: "+1w",
+		changeMonth: true,
+		numberOfMonths: 1,
+		showOtherMonths: true,
+		selectOtherMonths: true,
+		showAnim: "drop",
+		changeMonth: true,
+		changeYear: true,
+		yearRange: '1900:2999',
+		dateFormat: 'yy/mm/dd',
+		onSelect: function( selectedDate ) {
+			var option = this.id == "from2" ? "minDate" : "maxDate",
+				instance = jQuery( this ).data( "datepicker" ),
+				date = jQuery.datepicker.parseDate(
+					instance.settings.dateFormat ||
+					jQuery.datepicker._defaults.dateFormat,
+					selectedDate, instance.settings );
+			dates.not( this ).datepicker( "option", option, date );
+		},
+		showButtonPanel: true
+	});
+});
+
+jQuery(function() {
+	jQuery(".datepicker").datepicker({
+		changeMonth: true,
+		numberOfMonths: 1,
+		showOtherMonths: true,
+		selectOtherMonths: true,
+		showAnim: "drop",
+		changeMonth: true,
+		changeYear: true,
+		yearRange: '1900:2999',
+		dateFormat: 'yy/mm/dd',
+		showButtonPanel: true
+	});
+});
